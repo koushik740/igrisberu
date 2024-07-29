74104e821679fd931c3ee4998b37a1f493293c76
koushik740 <koushikreddy2s02@gmail.com>
2024-07-29 15:30:26 +0530

    b  b bcnb

--- pip.py
+++ pip.py
@@ -1,1 +1,5 @@
-print("data")
+import pandas as p
+d=p.DataFrame([[2,5,6],[4,6,3],[5,7,8]],columns=["maths","java","python"])
+print(d)
+c=d.agg(['min','max','sum','count','mean','median','std','size'])
+print(c)
