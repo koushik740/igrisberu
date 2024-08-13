8490c44ea9d9dc6d08209d146823eab7fa3e95ac
koushik740 <koushikreddy2s02@gmail.com>
2024-08-13 19:01:30 +0530

    kk

--- lamda.py
+++ lamda.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from functools import reduce
-data={'numbers':[1,2,3,4,5],'letters':['A','B','C','D','E']}
+data={'number':[1,2,3,4,5],'letters':['a','b','c','d','e']}
 df=pd.DataFrame(data)
-sq=df['numbers'].map(lambda x:x**2)
-ev=list(filter(lambda x:x%2==0,df['numbers']))
-po=reduce(lambda x,y:x*y,df['numbers'])
-print("\n map for squarting:\n",sq)
-print("\n reduce for product:\n",po)
-print("\n filter for even:\n",ev)
+sq=df['number'].map(lambda x:x**2)
+ev=list(filter(lambda x:x%2==0,df['number']))
+po=reduce(lambda x,y:x*y,df['number'])
+print('DataFrame:',df)
+print("\nmap for squaring:\n",sq)
+print("reduce for product:\n",po)
+print("\nfilter for even numbers:\n",ev)
